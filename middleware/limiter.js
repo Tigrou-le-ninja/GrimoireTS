2914715e8de7aeb906ccfd13b0e44ca962aa6878
Théo Sommacal <t.somm@yahoo.fr>
2025-11-21 16:29:20 +0100

    Typo limiter

--- middleware/limiter.js
+++ middleware/limiter.js
@@ -2,13 +2,13 @@ const { rateLimit } = require("express-rate-limit");
 
 const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 20, // limit each IP to 100 requests per windowMs
+  max: 20, // limit each IP to 20 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
 
 const authLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 15 minutes
-  max: 3, // limit each IP to 5 requests per windowMs
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 3, // limit each IP to 3 requests per windowMs
   message: "Too many login attempts from this IP, please try again later.",
 });
 
