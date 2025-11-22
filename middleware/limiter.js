50f555db5b9d1364b9b584f6f031fac3cb5239cf
Théo Sommacal <t.somm@yahoo.fr>
2025-11-22 01:18:23 +0100

    Assouplissement globalLimiter

--- middleware/limiter.js
+++ middleware/limiter.js
@@ -2,7 +2,7 @@ const { rateLimit } = require("express-rate-limit");
 
 const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 20, // limit each IP to 20 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
 
